f25132ec6e4defedc9a949b520a0bd512a119d6e
Rafael Mateus <rafael.mateus.silva@hotmail.com>
2017-10-17 10:46:19 -0200

    Adicionado controller de Tickets

---
+++ ITicket/Controllers/TicketsController.cs
@@ -0,0 +1,109 @@
+﻿using ITicket.Contexts;
+using ITicket.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ITicket.Controllers
+{
+    public class TicketsController : Controller
+    {
+        private EFContext context = new EFContext();
+        // GET: Usuarios
+        public ActionResult Index()
+        {
+            return View(context.Tickets.OrderBy(c => c.CodigoTicket));
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Ticket ticket)
+        {
+            context.Tickets.Add(ticket);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ticket ticket = context.Tickets.Find(id);
+            if (ticket == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ticket);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Ticket ticket)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Entry(ticket).State = EntityState.Modified;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(ticket);
+        }
+
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Ticket ticket = context.Tickets.Find(id);
+
+            if (ticket == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ticket);
+        }
+
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Ticket ticket = context.Tickets.Find(id);
+
+            if (ticket == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(ticket);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(long id)
+        {
+
+            Ticket ticket = context.Tickets.Find(id);
+            context.Tickets.Remove(ticket);
+            context.SaveChanges();
+            TempData["Message"] = "Ticket" + " " + ticket.CodigoTicket + " " + "Foi Removido com sucesso!";
+            return RedirectToAction("Index");
+        }
+
+    }
+}
