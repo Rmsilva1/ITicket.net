6dd14555741194f6198049037b9f41c71231f170
Rafael Mateus <Rafael.Mateus.Silva@hotmail.com>
2017-11-01 22:54:48 -0200

    code added

---
+++ ITicket/Controllers/ProdutosController.cs
@@ -0,0 +1,108 @@
+﻿using ITicket.Contexts;
+using ITicket.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ITicket.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        private EFContext context = new EFContext();
+
+        public ActionResult Index()
+        {
+            return View(context.Produtos.OrderBy(c => c.Nome));
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Produto produto)
+        {
+            context.Produtos.Add(produto);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = context.Produtos.Find(id);
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produto);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Entry(produto).State = EntityState.Modified;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(produto);
+        }
+
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Produto produto = context.Produtos.Find(id);
+
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produto);
+        }
+
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Produto produto = context.Produtos.Find(id);
+
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(produto);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(long id)
+        {
+
+            Produto produto = context.Produtos.Find(id);
+            context.Produtos.Remove(produto);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
